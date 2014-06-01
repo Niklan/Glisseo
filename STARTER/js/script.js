e91f1db58c14980741296d7a11ecf6d9adc979de
Niklan <niklanrus@gmail.com>
2014-06-01 23:38:01 +0600

    Created subtheme to fast start.

---
+++ STARTER/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * @file
+ * Example of JavaScript file for theme.
+ * You can edit it or write on your own.
+ */
+(function ($, Drupal, window, document, undefined) {
+
+    // Your code here.
+
+})(jQuery, Drupal, this, this.document);
