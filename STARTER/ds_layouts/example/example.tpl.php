6e4d7b971754a526a1d7dabf70b78bd2d4697c94
Niklan <niklanrus@gmail.com>
2015-02-22 15:45:51 +0500

    Changes created during dru.io theme development.

--- STARTER/ds_layouts/example/example.tpl.php
+++ STARTER/ds_layouts/example/example.tpl.php
@@ -1,11 +1,21 @@
-<?php if ($left): ?>
-  <div class="ds-left<?php print $left_classes; ?>">
-    <?php print $left; ?>
-  </div>
-<?php endif; ?>
+<?php
+/**
+ * @file
+ * Question full layout.
+ */
+?>
+<<?php print $layout_wrapper; ?> <?php print $layout_attributes; ?> class="<?php print $clean_classes; ?>">
+  <?php print render($title_suffix['contextual_links']); ?>
 
-<?php if ($right): ?>
-  <div class="ds-right<?php print $right_classes; ?>">
-    <?php print $right; ?>
-  </div>
-<?php endif; ?>
+  <?php if ($ds_left): ?>
+    <<?php print $ds_left_wrapper; ?> class="<?php print $ds_left_classes; ?>">
+      <?php print $ds_left; ?>
+    </<?php print $ds_left_wrapper; ?>>
+  <?php endif; ?>
+
+  <?php if ($ds_right): ?>
+    <<?php print $ds_right_wrapper; ?> class="<?php print $ds_right_classes; ?>">
+      <?php print $ds_right; ?>
+    </<?php print $ds_right_wrapper; ?>>
+  <?php endif; ?>
+</<?php print $layout_wrapper; ?>>
