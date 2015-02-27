974af6282c312471c31cae8d8a5695d2174c95ac
Niklan <niklanrus@gmail.com>
2015-02-27 18:50:57 +0500

    Fix Display Suite boilerplate template.

--- STARTER/ds_layouts/example/example.tpl.php
+++ STARTER/ds_layouts/example/example.tpl.php
@@ -7,15 +7,15 @@
 <<?php print $layout_wrapper; ?> <?php print $layout_attributes; ?> class="<?php print $clean_classes; ?>">
   <?php print render($title_suffix['contextual_links']); ?>
 
-  <?php if ($ds_left): ?>
-    <<?php print $ds_left_wrapper; ?> class="<?php print $ds_left_classes; ?>">
-      <?php print $ds_left; ?>
-    </<?php print $ds_left_wrapper; ?>>
+  <?php if ($left): ?>
+    <<?php print $left_wrapper; ?> class="<?php print $left_classes; ?>">
+      <?php print $left; ?>
+    </<?php print $left_wrapper; ?>>
   <?php endif; ?>
 
-  <?php if ($ds_right): ?>
-    <<?php print $ds_right_wrapper; ?> class="<?php print $ds_right_classes; ?>">
-      <?php print $ds_right; ?>
-    </<?php print $ds_right_wrapper; ?>>
+  <?php if ($right): ?>
+    <<?php print $right_wrapper; ?> class="<?php print $right_classes; ?>">
+      <?php print $right; ?>
+    </<?php print $right_wrapper; ?>>
   <?php endif; ?>
 </<?php print $layout_wrapper; ?>>
