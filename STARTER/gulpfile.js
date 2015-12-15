8a848d63c01aa35d17b5db41d814312a84e89493
Niklan <niklanrus@gmail.com>
2015-12-15 12:18:39 +0500

    Added gulp-sourcemaps for SASS mapping

--- STARTER/gulpfile.js
+++ STARTER/gulpfile.js
@@ -1,20 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
-    gulp.src('./styles/scss/styles.scss')
-        .pipe(sass({
-            errLogToConsole: true,
-            sourceComments: 'normal'
-        }))
-        .pipe(gulp.dest('./styles/css/'));
+  gulp.src('./styles/scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./styles/css/'));
 });
 
 gulp.task('watch', function () {
-    gulp.start('sass');
-    gulp.watch('./styles/scss/**', function () {
-        setTimeout(function () {
-            gulp.start('sass');
-        }, 200);
-    });
+  gulp.start('sass');
+  gulp.watch('./styles/scss/**', function () {
+    setTimeout(function () {
+      gulp.src('./styles/scss/styles.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./styles/css/'));
+    }, 200);
+  });
 });
