a76ee3c9ec1f44e173ae9e806ab3553531a5de7c
Niklan <niklanrus@gmail.com>
2016-10-07 18:52:56 +0500

    Rewrited gulpfile.js and updated dependencies versions

--- STARTER/gulpfile.js
+++ STARTER/gulpfile.js
@@ -1,21 +1,24 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
+var paths = {
+  scss: './styles/scss/**',
+  css: './styles/scss/styles.scss'
+};
+
 gulp.task('sass', function () {
-  gulp.src('./styles/scss/styles.scss')
+  gulp.src(paths.css)
     .pipe(sourcemaps.init())
-    .pipe(sass())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./styles/css/'));
 });
 
 gulp.task('watch', function () {
-  gulp.start('sass');
-  gulp.watch('./styles/scss/**', function () {
-    setTimeout(function () {
-      gulp.start('sass');
-    }, 200);
-  });
+  gulp.watch(paths.scss, ['sass']);
 });
 
+gulp.task('default', ['watch', 'sass']);
