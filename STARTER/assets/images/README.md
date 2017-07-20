093294f6bdf4948e4d186a560e75e055caeb3ed0
Niklan <niklanrus@gmail.com>
2017-07-20 13:06:48 +0500

    - Added missed image folder. - Removed configration to base theme which not used. - Removed unused form. Will be recreated somehow. - Removed preprocess region, cuz all now in templates.

---
+++ STARTER/assets/images/README.md
@@ -0,0 +1,3 @@
+# Images
+
+Folder for images required by theme.
--- theme-settings.php
+++ theme-settings.php
@@ -10,16 +10,4 @@ function glisseo_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\Form
   if (isset($form_id)) {
     return;
   }
-
-  $form['region_settings'] = [
-    '#type' => 'fieldset',
-    '#title' => t('Region settings'),
-  ];
-
-  $form['region_settings']['glisseo_sidebars_add_classes'] = [
-    '#type' => 'checkbox',
-    '#title' => t('Add classes with sidebar names to <body> element'),
-    '#description' => t('Any sidebar_* region rendered on the page will be added as class layout-sidebar-NAME.'),
-    '#default_value' => theme_get_setting('glisseo_sidebars_add_classes'),
-  ];
 }
