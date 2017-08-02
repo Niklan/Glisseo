28ed67b8f0ce83d6de7746d462bd31082ece8cb7
Niklan <niklanrus@gmail.com>
2017-08-02 11:53:51 +0500

    Fixed and improvemd gulpfile.js

--- STARTER/gulpfile.js
+++ STARTER/gulpfile.js
@@ -1,24 +1,26 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 
-var paths = {
+const paths = {
   scss: 'assets/scss/**',
   css: 'assets/scss/styles.scss'
 };
 
 gulp.task('sass', function () {
   return gulp.src(paths.css)
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer({
+      .pipe(sourcemaps.init())
+      .pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
       }))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('./assets/css/'));
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write('./maps'))
+      .pipe(gulp.dest('./assets/css/'))
+      .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
