5e72c7317ab423b178299b50ae7b81178ce9f6ce
Niklan <niklanrus@gmail.com>
2022-05-19 20:32:13 +0500

    Remove all leftovers from node.js.

--- STARTER/scripts/gulpfile.esm.js
+++
@@ -1,6 +0,0 @@
-import {compileCss} from "./css/compile";
-import {watchCss} from "./css/watch";
-import {parallel} from 'gulp';
-
-export const compile = parallel(compileCss);
-export const watch = parallel(watchCss);
