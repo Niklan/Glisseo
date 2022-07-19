aebabf2486bedcf1958913f60354101993425909
git <git@3644742.no-reply.drupal.org>
2022-07-19 17:31:14 +0500

    Issue #3297312 by Project Update Bot: Automated Drupal 10 compatibility fixes

--- theme-settings.php
+++ theme-settings.php
@@ -1,11 +1,12 @@
 <?php
 
+use Drupal\Core\Form\FormStateInterface;
+
 /**
  * @file
  * Custom theme settings.
  */
-
-function glisseo_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStateInterface &$form_state, $form_id = NULL) {
+function glisseo_form_system_theme_settings_alter(&$form, FormStateInterface &$form_state, $form_id = NULL) {
   // Work-around for a core bug affecting admin themes. See issue #943212.
   if (isset($form_id)) {
     return;
